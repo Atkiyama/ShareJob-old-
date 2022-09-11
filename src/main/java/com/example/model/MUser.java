f25465ad1c808013ba3ede0a73cdda33c62ef276
KeigoMatsumura <mahboonuts4951@gmail.com>
2022-09-12 05:51:25 +0900

    implemented chapeter13

--- src/main/java/com/example/model/MUser.java
+++ src/main/java/com/example/model/MUser.java
@@ -1,11 +1,37 @@
 package com.example.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.util.Date;
+
 /**
  * ユーザ情報を定義するクラス
  * 今回は userId とpasswordだけでいい
  * @author akiyamashuuhei
  *
+ * @author keigomatsumura
+ * @date 2022/9/11
+ *
  */
+@Data
+@Entity
+@Table(name="m_user")
 public class MUser {
-
+    @Id
+    private String userId;
+    private String password;
+//    private String userName;
+//    private Date birthday;
+//    private Integer age;
+//    private Integer gender;
+//    private Integer departmentId;
+//    private String role;
+//    @Transient
+//    private Department department;
+//    @Transient
+//    private List<Salary> salaryList;
 }
--- src/main/java/com/example/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -1,10 +1,89 @@
 package com.example.service.impl;
 
+import com.example.model.MUser;
+import com.example.repository.UserRepository;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * 13章を参照
  * @author akiyamashuuhei
  *
+ * @author keigomatsumura
+ * @date 2022/9/11
+ *
  */
-public class UserServiceImpl {
+@Service
+@Primary
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository repository;
 
+    @Autowired
+    private PasswordEncoder encoder;
+
+    //ユーザ登録
+    @Transactional
+    @Override
+    public void signup(MUser user) {
+        // 存在チェック
+        boolean exists = repository.existsById(user.getUserId());
+        if(exists){
+            throw new DataAccessException("ユーザが既に存在"){};
+        }
+
+//        user.setDepartmentId(1);
+//        user.setRole("Role_GENERAL");
+
+        // パスワード暗号化
+        String rawPassword = user.getPassword();
+        user.setPassword(encoder.encode(rawPassword));
+
+        // insert
+        repository.save(user);
+    }
+
+    // ユーザ取得
+    @Override
+    public List<MUser> getUsers(MUser user) {
+        return repository.findAll();
+    }
+
+    // ユーザ取得（１件）
+    @Override
+    public MUser getUserOne(String userId) {
+        Optional<MUser> option = repository.findById(userId);
+        MUser user = option.orElse(null);
+        return user;
+    }
+
+    // ユーザ更新
+    @Transactional
+    @Override
+    public void updateUserOne(String userId, String password, String userName) {
+
+    }
+
+    // ユーザー削除（１件）
+    @Transactional
+    @Override
+    public void deleteUserOne(String userId, String password, String userName) {
+        repository.deleteById(userId);
+    }
+
+    // ログインユーザ取得
+    @Override
+    public MUser getLoginUser(String userId) {
+        Optional<MUser> option = repository.findById(userId);
+        MUser user = option.orElse(null);
+        return user;
+    }
 }
