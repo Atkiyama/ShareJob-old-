a76aeaa4a381cde0ff7f6f1a3aaeb0eea50cda0e
秋山周平 <akiyamashuuhei@akiyamashuuheinoMacBook-Pro.local>
2022-09-12 12:22:48 +0900

    担当箇所完了

--- src/main/java/com/example/aspect/ErrorAspect.java
+++ src/main/java/com/example/aspect/ErrorAspect.java
@@ -1,5 +1,22 @@
 package com.example.aspect;
 
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Component
+@Slf4j
 public class ErrorAspect {
 
+    @AfterThrowing(value = "execution(* *..*..*(..)) &&"
+            + "(bean(*Controller) || bean(*Service) || bean(*Repository))",
+            throwing = "ex")
+    public void throwingNull(DataAccessException ex) {
+        // 例外処理の内容（ログ出力）
+        log.error("DataAccessExceptionが発生しました");
+    }
 }
--- src/main/java/com/example/aspect/LogAspect.java
+++ src/main/java/com/example/aspect/LogAspect.java
@@ -1,5 +1,63 @@
 package com.example.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Component
+@Slf4j
 public class LogAspect {
 
+    /**
+     * サービスの実行前にログ出力する.
+     * 対象:[UserService]をクラス名に含んでいる.
+     */
+    @Before("execution(* *..*.*UserService.*(..))")
+    public void startLog(JoinPoint jp) {
+        log.info("メソッド開始: " + jp.getSignature());
+    }
+
+    /**
+     * サービスの実行後にログ出力する.
+     * 対象:[UserService]をクラス名に含んでいる.
+     */
+    @After("execution(* *..*.*UserService.*(..))")
+    public void endLog(JoinPoint jp) {
+        log.info("メソッド終了: " + jp.getSignature());
+    }
+
+    /** コントローラーの実行前後にログ出力する */
+    //@Around("bean(*Controller)")
+    //@Around("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    @Around("@within(org.springframework.stereotype.Controller)")
+    public Object startLog(ProceedingJoinPoint jp) throws Throwable {
+
+        // 開始ログ出力
+        log.info("メソッド開始: " + jp.getSignature());
+
+        try {
+            // メソッド実行
+            Object result = jp.proceed();
+
+            // 終了ログ出力
+            log.info("メソッド終了: " + jp.getSignature());
+
+            // 実行結果を呼び出し元に返却
+            return result;
+
+        } catch (Exception e) {
+            // エラーログ出力
+            log.error("メソッド異常終了: " + jp.getSignature());
+
+            // エラーの再スロー
+            throw e;
+        }
+    }
 }
