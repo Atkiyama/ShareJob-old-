38d9abaac985a92acf0d15420e0360fc49075ead
KeigoMatsumura <mahboonuts4951@gmail.com>
2022-11-28 06:49:21 +0900

    added signup views and showLoginInfo views

---
+++ src/frontend/src/views/ShowLoginInfo.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="m-5 p-4">
+        <div class="p-3 text-center">
+            <h1>登録が完了しました！</h1>
+        </div>
+        <h4>次回ログインからは、下記IDとパスワードでログインしてください。</h4>
+    </div>
+</template>
