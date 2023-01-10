238d811cf1edef7c44838128f9017ac8be57b444
Atkiyama <thisisatkiyama@gmail.com>
2023-01-10 13:32:08 +0900

    認証デモ追加

---
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -0,0 +1,127 @@
+package com.example.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.web.filter.GenericFilterBean;
+
+import com.example.demo.repository.UserRepository;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Value("${security.secret-key:secret}")
+    private String secretKey = "secret";
+
+    /**
+     * ログイン時の全体的な処理
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+            // AUTHORIZE
+        /**
+         * 権限によってどこにアクセスできるかを区別するため認証設定時にはパスの大掛かりな変更が必要
+         */
+            .authorizeRequests()
+                .mvcMatchers("/hello/**")
+                    .permitAll()
+                .mvcMatchers("/user/**")
+                    .hasRole("USER")
+                .mvcMatchers("/admin/**")
+                    .hasRole("ADMIN")
+                .anyRequest()
+                    .authenticated()
+            .and()
+            // EXCEPTION
+            .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint())
+                .accessDeniedHandler(accessDeniedHandler())
+            .and()
+            // LOGIN
+            .formLogin()
+                .loginProcessingUrl("/login").permitAll()
+                	//IDを設定
+                    .usernameParameter("email")
+                    //パスワードを設定
+                    .passwordParameter("pass")
+                .successHandler(authenticationSuccessHandler())
+                .failureHandler(authenticationFailureHandler())
+            .and()
+            // LOGOUT
+            .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessHandler(logoutSuccessHandler())
+            .and()
+            // CSRF いずれはトークンを突っ込む必要がある?
+            .csrf()
+                .disable()
+            // AUTHORIZE
+            .addFilterBefore(tokenFilter(), UsernamePasswordAuthenticationFilter.class)
+            // SESSION
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            ;
+        // @formatter:on
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.eraseCredentials(true)
+                .userDetailsService(simpleUserDetailsService())
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean("simpleUserDetailsService")
+    UserDetailsService simpleUserDetailsService() {
+        return new SimpleUserDetailsService(userRepository);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    GenericFilterBean tokenFilter() {
+        return new SimpleTokenFilter(userRepository, secretKey);
+    }
+
+    AuthenticationEntryPoint authenticationEntryPoint() {
+        return new SimpleAuthenticationEntryPoint();
+    }
+
+    AccessDeniedHandler accessDeniedHandler() {
+        return new SimpleAccessDeniedHandler();
+    }
+
+    AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new SimpleAuthenticationSuccessHandler(secretKey);
+    }
+
+    AuthenticationFailureHandler authenticationFailureHandler() {
+        return new SimpleAuthenticationFailureHandler();
+    }
+
+    LogoutSuccessHandler logoutSuccessHandler() {
+        return new HttpStatusReturningLogoutSuccessHandler();
+    }
+
+}
---
+++ src/main/java/com/example/security/SimpleAuthenticationSuccessHandler.java
@@ -0,0 +1,108 @@
+package com.example.demo.security;
+
+import java.util.Date;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 認証が成功した時の処理を実装したクラス
+ */
+@Slf4j
+public class SimpleAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+/**
+ * 使用するアルゴリズムとキーをセットした値
+ */
+  final private Algorithm algorithm;
+
+  /**
+   * 秘密鍵の生成をするクラス
+   * フィールドのアルゴリズムを用意する
+   * @param secretKey
+   */
+  public SimpleAuthenticationSuccessHandler(String secretKey) {
+    Objects.requireNonNull(secretKey, "secret key must be not null");
+    this.algorithm = Algorithm.HMAC512(secretKey);
+  }
+
+  /**
+   * トークンの持続時間
+   */
+  private static final Long EXPIRATION_TIME = TimeUnit.MINUTES.toMillis(10L);
+
+  @Override
+  public void onAuthenticationSuccess(HttpServletRequest request,
+                                      HttpServletResponse response,
+                                      Authentication auth) {
+    if (response.isCommitted()) {
+      log.info("Response has already been committed.");
+      return;
+    }
+    setToken(response, generateToken(auth));
+    response.setStatus(HttpStatus.OK.value());
+    clearAuthenticationAttributes(request);
+  }
+
+  /**
+   * トークンを生成するメソッド
+   * @param auth
+   * @return
+   */
+  private String generateToken(Authentication auth) {
+    SimpleLoginUser loginUser = (SimpleLoginUser) auth.getPrincipal();
+    //日付を取得
+    Date issuedAt = new Date();
+    //トークンが使える開始時刻
+    Date notBefore = new Date(issuedAt.getTime());
+    //トークンの終了時刻
+    Date expiresAt = new Date(issuedAt.getTime() + EXPIRATION_TIME);
+    //トークン生成
+    String token = JWT.create()
+        .withIssuedAt(issuedAt)
+        .withNotBefore(notBefore)
+        .withExpiresAt(expiresAt)
+        .withSubject(loginUser.getUser().getId().toString())
+        .sign(this.algorithm);
+    log.debug("generate token : {}", token);
+    return token;
+  }
+
+  /**
+   * 
+   * レスポンスにトークンをセットするメソッド
+   * @param response
+   * @param token
+   */
+  private void setToken(HttpServletResponse response, String token) {
+    response.setHeader("Authorization", String.format("Bearer %s", token));
+  }
+
+  /**
+   * Removes temporary authentication-related data which may have been stored in the
+   * session during the authentication process.
+   * 
+   * 認証処理中にセッションに保存された一時的な認証関連データを削除します。
+   * 認証処理中にセッションに保存された可能性のある一時的な認証関連データを削除します。
+   */
+  private void clearAuthenticationAttributes(HttpServletRequest request) {
+    HttpSession session = request.getSession(false);
+    if (session == null) {
+      return;
+    }
+    session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+  }
+
+}
